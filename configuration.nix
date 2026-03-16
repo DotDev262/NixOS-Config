680a43cf6eea6b3130f58e743e3128857386314b
DotDev262 <dotdev262@gmail.com>
2026-03-16 21:43:17 +0530

    refactor(nixos): Update renamed GNOME and GDM service options
    Updated 'services.xserver.desktopManager.gnome.enable' to 'services.desktopManager.gnome.enable' and 'services.xserver.displayManager.gdm.enable' to 'services.displayManager.gdm.enable' in configuration.nix to align with recent NixOS option renames.

--- configuration.nix
+++ configuration.nix
@@ -46,17 +46,14 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = false;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
