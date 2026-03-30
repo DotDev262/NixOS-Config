50e91275432ccce6c930e52b2e22730d3dd285f1
DotDev262 <dotdev262@gmail.com>
2026-03-30 22:20:31 +0530

    feat: add arch-shell function for pure Arch Linux environment

--- modules/shell.nix
+++ modules/shell.nix
@@ -12,6 +12,7 @@
     };
     functions = {
       sudopath = "sudo env \"PATH=$PATH\" $argv";
+      arch-shell = "/usr/bin/env -i HOME=\"$HOME\" TERM=\"$TERM\" PATH=\"/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin\" /usr/bin/bash --norc --noprofile -c 'unset NIX_PROFILES NIX_PATH NIX_SSL_CERT_FILE __ETC_PROFILE_NIX_SOURCED LG_CONFIG_FILE GLAMOUR_STYLE XCURSOR_PATH LOCALE_ARCHIVE_2_27 NH_FLAKE NIXOS_OZONE_WL DBUS_SESSION_BUS_ADDRESS XDG_DATA_DIRS TERMINFO_DIRS GPG_TTY; exec /usr/bin/fish --no-config'";
     };
     interactiveShellInit = ''
       set -g fish_greeting
@@ -27,5 +28,6 @@
     hmn = "home-manager news --flake /home/aryan/nixos-config#aryan";
     zen = "nixGLIntel zen";
     vivaldi = "nixGLIntel vivaldi";
+    arch-shell = "env -i HOME=\"$HOME\" TERM=\"$TERM\" PATH=\"/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin\" /usr/bin/bash --norc --noprofile";
   };
 }
